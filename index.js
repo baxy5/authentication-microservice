181c5d64a548fd4df27db10c44f4de02f12b3950
baxy5 <bakos.soda@gmail.com>
2022-10-31 11:01:17 +0100

    routing

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
 const client = require("./database");
 
+app.set("view engine", "ejs");
+
+// routing
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.use((req, res) => {
+  res.status(404).render("404");
+});
+
+// server
 app.listen(8000, () => {
   console.log("Server running on port 8000.");
   client.connect((err) => {
