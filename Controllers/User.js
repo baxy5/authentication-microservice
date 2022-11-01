73db634cbe8b35a2fb6315a9198f1aaea371a028
baxy5 <bakos.soda@gmail.com>
2022-11-01 15:43:23 +0100

    signup logic

--- Controllers/User.js
+++ Controllers/User.js
@@ -1,16 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const bycrpt = require("bcrypt");
+const client = require("../database");
 
 router.post("/register", (req, res, next) => {
-  const { email, name, password } = req.body;
+  const { email, password } = req.body;
 
   const hashedPassword = bycrpt.hashSync(password, 10);
 
   // TODO: Check email in database
-  // TODO: Save credentials to database
+  client.query("SELECT email FROM credentials", (err, data) => {
+    if (err) console.log(err);
+
+    data.rows.forEach((row) => {
+      console.log("Searching for email.");
+      if (row.email == email) {
+        res.send("This email is already exist. Please login.");
+        console.log("Email is already exist.");
+      } else {
+        // TODO: Save credentials to database
+        console.log("Email doesn't exist.");
+        client.query(
+          `INSERT INTO credentials (email,password) VALUES (${email}, ${hashedPassword})`,
+          (err, data) => {
+            if (err) console.log(err);
+
+            console.log("Credentials was added.");
+          }
+        );
+      }
+    });
+  });
 });
 
 router.post("/login", (req, res, next) => {
   // TODO: Check user in database
 });
+
+module.exports = router;
--- index.js
+++ index.js
@@ -3,18 +3,49 @@ const app = express();
 const bodyParser = require("body-parser");
 const client = require("./database");
 require("dotenv").config();
+const bycrpt = require("bcrypt");
 
 app.use(bodyParser.json());
+client.connect();
 
-// TODO: Log data via routing
+const userRouter = require("./Controllers/User");
+app.use("/register", userRouter);
+
 app.get("/", (req, res) => {
-  client.connect();
-
   client.query("SELECT email, password FROM credentials", (err, data) => {
     if (err) console.log(err);
 
     res.send(data.rows);
-    client.end();
+  });
+});
+
+app.post("/register", (req, res, next) => {
+  const { email, password } = req.body;
+
+  const hashedPassword = bycrpt.hashSync(password, 10);
+
+  // TODO: Check email in database
+  client.query("SELECT email FROM credentials", (err, data) => {
+    if (err) console.log(err);
+
+    data.rows.forEach((row) => {
+      console.log("Searching for email.");
+      if (row.email == email) {
+        res.send("This email is already exist. Please login.");
+        console.log("Email is already exist.");
+      } else {
+        // TODO: Save credentials to database
+        console.log("Email doesn't exist.");
+        client.query(
+          `INSERT INTO credentials (email,password) VALUES (${email}, ${hashedPassword})`,
+          (err, data) => {
+            if (err) console.log(err);
+
+            console.log("Credentials was added.");
+          }
+        );
+      }
+    });
   });
 });
 
